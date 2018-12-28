c8f9aa8660ace842c9ff2a087a600898eed8f836
wellingtonx <wellingtonex@gmail.com>
2018-12-28 20:27:34 -0200

    fazendo a diretiva escutar um evento sobre o componente html para mudar sua aparencia de acordo com o evento

--- src/app/diretivas/campo-colorido.directive.ts
+++ src/app/diretivas/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
@@ -8,8 +8,14 @@ export class CampoColoridoDirective {
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
-  ) { 
+  ) {}
+
+  @HostListener('focus') ganharFoco() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  }
+
+  @HostListener('blur') perderFoco() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 
 }
