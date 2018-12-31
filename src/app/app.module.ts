88b9c4a85c170f750ce75a08ded3a3498c9fc8dd
wellingtonx <wellingtonex@gmail.com>
2018-12-30 22:53:05 -0200

    criando um core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
+import { CoreModule } from './core/core.module';
 import { LacamentosModule } from './lacamentos/lacamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     LacamentosModule,
     PessoasModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
