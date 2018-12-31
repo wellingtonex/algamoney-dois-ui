74b9ed1eafd246e2bcb995a70f98f7ec002d3408
wellingtonx <wellingtonex@gmail.com>
2018-12-30 22:35:46 -0200

    criando modulo de pessoas e movendo os componentes relacionados a pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import {InputTextModule} from 'primeng/components/inputtext/inputtext';
-import {ButtonModule} from 'primeng/components/button/button';
-import {DataTableModule} from 'primeng/components/datatable/datatable';
-import {TooltipModule} from 'primeng/components/tooltip/tooltip';
-import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
-import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
-import {DropdownModule} from 'primeng/components/dropdown/dropdown';
-import {InputMaskModule} from 'primeng/primeng';
-
-import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
 import { CampoColoridoDirective } from './diretivas/campo-colorido.directive';
 
-import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
 import { MessageComponent } from './message/message.component';
 import { LacamentosModule } from './lacamentos/lacamentos.module';
-
-
-
+import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PessoasPesquisaComponent,
-    CampoColoridoDirective,
-    PessoasCadastroComponent,
+    CampoColoridoDirective,    
     MessageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    
-    InputTextModule,
-    ButtonModule,
-    DataTableModule,
-    TooltipModule,
-    InputTextareaModule,
-    SelectButtonModule,
-    DropdownModule,
-    CurrencyMaskModule,
-    InputMaskModule,
-    FormsModule,
-
-    LacamentosModule
+    LacamentosModule,
+    PessoasModule
 
   ],
   providers: [],
---
+++ src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import {ButtonModule} from 'primeng/components/button/button';
+import {DataTableModule} from 'primeng/components/datatable/datatable';
+import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import {InputTextareaModule} from 'primeng/components/inputtextarea/inputtextarea';
+import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
+import {DropdownModule} from 'primeng/components/dropdown/dropdown';
+import {InputMaskModule} from 'primeng/primeng';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { PessoaPesquisaGridComponent } from './pessoa-pesquisa-grid/pessoa-pesquisa-grid.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+
+    BrowserAnimationsModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TooltipModule,
+    InputTextareaModule,
+    SelectButtonModule,
+    DropdownModule,    
+    InputMaskModule
+  ],
+  declarations: [
+    PessoasPesquisaComponent,
+    PessoasCadastroComponent,
+    PessoaPesquisaGridComponent
+  ],
+  exports: [
+    PessoasPesquisaComponent,
+    PessoasCadastroComponent
+  ]
+})
+export class PessoasModule { }
