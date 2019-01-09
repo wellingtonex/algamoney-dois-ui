657138919f6f01e38ce7558a0ac6479bd6a75796
wellingtonx <wellingtonex@gmail.com>
2019-01-08 22:59:23 -0200

    corrigindo problema com locale pt-BR

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,8 @@
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 
 import { ConfirmationService } from 'primeng/components/common/api';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
@@ -18,6 +19,7 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component'
 import { AuthService } from 'app/seguranca/auth.service';
 import { NaoAutorizadoComponent } from './nao-autorizado.component';
 
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
@@ -46,7 +48,7 @@ import { NaoAutorizadoComponent } from './nao-autorizado.component';
     AuthService,
     ConfirmationService,
     JwtHelper,
-    { provide: LOCALE_ID, useValue: 'pt-BR' }
+    { provide: LOCALE_ID, useValue: 'pt' }
   ]
 })
 export class CoreModule { }
